a83d2edeb51adacc4d0c68cda24c831ec5656935
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-22 11:21:57 +0300

    + init

--- src/Bootloader.php
+++ src/Bootloader.php
@@ -3,9 +3,14 @@ declare(strict_types=1);
 
 namespace IfCastle\Monolog;
 
+use IfCastle\Application\Bootloader\BootloaderContextInterface;
 use IfCastle\Application\Bootloader\BootloaderExecutorInterface;
 use IfCastle\Application\Bootloader\BootloaderInterface;
+use IfCastle\Application\EngineRolesEnum;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
 use Psr\Log\LoggerInterface;
 
 final class Bootloader implements BootloaderInterface
@@ -13,12 +18,26 @@ final class Bootloader implements BootloaderInterface
     public function buildBootloader(BootloaderExecutorInterface $bootloaderExecutor): void
     {
         $bootloaderExecutor->getBootloaderContext()->getSystemEnvironmentBootBuilder()
-            ->bindObject(LoggerInterface::class, $this->buildLogger($bootloaderExecutor->getBootloaderContext()->getApplicationType()));
+            ->bindObject(
+                LoggerInterface::class,
+                $this->buildLogger($bootloaderExecutor->getBootloaderContext())
+            );
     }
     
-    private function buildLogger(string $applicationType): LoggerInterface
+    private function buildLogger(BootloaderContextInterface $bootloaderContext): LoggerInterface
     {
-        // @todo: Configure the logger.
-        return new Logger($applicationType);
+        $logger                     = new Logger($bootloaderContext->getApplicationType());
+        $configuration              = $bootloaderContext->getApplicationConfig()->findSection('logger');
+        $roles                      = $bootloaderContext->getExecutionRoles();
+        
+        if(in_array(EngineRolesEnum::CONSOLE->value, $roles, true)) {
+            $logHandler             = new StreamHandler('php://stdout');
+            $logHandler->pushProcessor(new PsrLogMessageProcessor);
+            $logHandler->setFormatter(new LineFormatter);
+            
+            $logger->pushHandler($logHandler);
+        }
+        
+        return $logger;
     }
 }
